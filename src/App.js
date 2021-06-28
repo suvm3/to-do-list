09323c6b734b29da0e6585392f50812d6c086384
personal <shubham20154129@gmail.com>
2021-06-28 21:41:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import React, {useState} from "react"
+
+
+function Todo({key, index, list, removeTask, completeTask}) {
+  return (
+    <div className='todo' style={{textDecoration: list.isCompleted ? 'line-through': ''}}>
+      {list.text}
+      <div>
+        <button className='btn btn-primary' onClick={()=> removeTask(index)}>Remove</button>
+        <button className='btn btn-secondary' onClick={() => completeTask(index)}>Completed</button>
+      </div>
+    </div>
+  )
+}
+
+function TodoForm({addTask}) {
+  const [value, setValue] = useState('');
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    if(value === '') {
+      alert('please add a task');
+    } else {
+      addTask(value);
+      setValue('');
+    }
+  }
+  return (
+    <form onSubmit={handleSumbit} >
+      <div className='mb-3'>
+        <label for='addTask' className='form-label'>Add Task</label>
+        <input type='text' className='form-control' id='addTask' value={value} onChange={(e) => setValue(e.target.value)} />
+        <button type='submit' className="btn btn-success">Submit</button>
+      </div>
+    </form>
+  )
+}
 
 function App() {
+  const [list, setList] = useState([
+    { text: 'Learn React Js', isCompleted: false},
+    { text: 'Learn Node', isCompleted: false},
+    { text: 'Lose 5 kg in 3 months', isCompleted: false}
+  ])
+
+  const addTask = (newTask) => {
+    const newObj = {text: newTask};
+    const newList = [...list, newObj];
+    setList(newList);
+  }
+
+  const removeTask = (index) => {
+    const newList = [...list];
+    newList.splice(index, 1);
+    setList(newList);
+  }
+
+  const completeTask = (index) => {
+    const newList = [...list];
+    newList[index].isCompleted = true;
+    setList(newList)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='todo__list'>
+        {list.map((item, index) => (
+          <Todo
+            key={index}
+            index={index}
+            list={item}
+            removeTask={removeTask}
+            completeTask={completeTask}
+          />
+        ))}
+      </div>
+      <TodoForm addTask={addTask} />
     </div>
   );
 }
